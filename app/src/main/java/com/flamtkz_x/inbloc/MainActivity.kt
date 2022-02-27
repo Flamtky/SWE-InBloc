a8ab3ab7e04af7e06102608d30ff3e9c4f238a30
Flamtky <n.schumann@rz-online.de>
2022-02-27 21:20:33 +0100

    init andriod project in kotlin

---
+++ app/src/main/java/com/flamtkz_x/inbloc/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.flamtkz_x.inbloc
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
