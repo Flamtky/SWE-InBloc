d35c2ebbf62e31ee9924d4ed3c97fb1c9bfc9e94
Flamtky <n.schumann@rz-online.de>
2022-02-28 23:10:18 +0100

    renamed main to login activity

--- app/src/main/java/com/flamtkz_x/inbloc/LoginActivity.kt
+++ app/src/main/java/com/flamtkz_x/inbloc/LoginActivity.kt
@@ -3,7 +3,7 @@ package com.flamtkz_x.inbloc
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
