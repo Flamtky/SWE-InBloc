6d3c1c0e02697c5050d1a6a9c44b8d2d08a3a39d
Flamtky <n.schumann@rz-online.de>
2022-02-28 23:31:47 +0100

    renamed activity layout | added btn logic

--- app/src/main/java/com/flamtkz_x/inbloc/LoginActivity.kt
+++ app/src/main/java/com/flamtkz_x/inbloc/LoginActivity.kt
@@ -1,11 +1,42 @@
 package com.flamtkz_x.inbloc
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
+
+    private val switchToSignUp: Button by lazy { findViewById<Button>(R.id.signUpButtonMain) }
+    private val loginBtn: Button by lazy { findViewById<Button>(R.id.loginbuttonMain) }
+
+    private val emailField by lazy { findViewById<EditText>(R.id.emailMain) }
+    private val passwordField by lazy { findViewById<EditText>(R.id.passwordMain) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+
+        // Switch to Register Page
+        switchToSignUp.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Login Button
+        loginBtn.setOnClickListener {
+            login(emailField.text.toString(), passwordField.text.toString())
+        }
+    }
+
+    /**
+     * Login Function
+     * @param email Email
+     * @param password Password
+     * @return void
+     */
+    fun login(email: String, password: String) {
+        // TODO: Login Function
     }
 }
--- app/src/main/java/com/flamtkz_x/inbloc/RegisterActivity.kt
+++ app/src/main/java/com/flamtkz_x/inbloc/RegisterActivity.kt
@@ -1,11 +1,34 @@
 package com.flamtkz_x.inbloc
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class RegisterActivity : AppCompatActivity() {
+
+    private val switchToLoginBtn: Button by lazy { findViewById<Button>(R.id.signUpButtonLoginPage) }
+    private val registerBtn: Button by lazy { findViewById<Button>(R.id.signUpButtonRegister) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        // Switch to login page
+        switchToLoginBtn.setOnClickListener {
+            finish()
+        }
+
+        // Register Button
+        registerBtn.setOnClickListener {
+            register()
+        }
+    }
+
+    /**
+     * Register the user
+     * TODO: Implement
+     */
+    private fun register() {
+
     }
 }
