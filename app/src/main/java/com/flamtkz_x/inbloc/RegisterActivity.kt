ec324a09511153740456e8ca0e9f879658a8efe8
Flamtky <n.schumann@rz-online.de>
2022-02-28 23:09:39 +0100

    renamed register | added new strings

--- app/src/main/java/com/flamtkz_x/inbloc/RegisterActivity.kt
+++ app/src/main/java/com/flamtkz_x/inbloc/RegisterActivity.kt
@@ -1,9 +1,9 @@
 package com.flamtkz_x.inbloc
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class Register : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
