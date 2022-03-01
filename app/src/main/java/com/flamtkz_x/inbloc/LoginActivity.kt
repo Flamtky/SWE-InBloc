9f86c201742594b0ae70a9d1711be47aa71b413f
Flamtky <n.schumann@rz-online.de>
2022-03-01 12:35:47 +0100

    refactored login
    - added function for every aspect to improve the readability

--- app/src/main/java/com/flamtkz_x/inbloc/LoginActivity.kt
+++ app/src/main/java/com/flamtkz_x/inbloc/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.flamtkz_x.inbloc
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -39,7 +40,7 @@ class LoginActivity : AppCompatActivity() {
             // TODO: remove this, just for testing
             val email = auth.currentUser!!.email
             Toast.makeText(this, "Already logged in $email", Toast.LENGTH_SHORT).show()
-            auth.signOut() // TODO: remove this, just for testing
+            auth.signOut()
         }
 
         // Switch to Register Page
@@ -55,7 +56,35 @@ class LoginActivity : AppCompatActivity() {
     }
 
     /**
+     * Login Function
+     *
+     * Shows a progress bar while logging in and disables all fields. Uses [FirebaseAnalytics] to log
+     * @param email Email
+     * @param password Password (min 6 characters)
+     * @return void
+     */
+    fun login(email: String, password: String) {
+        if (!areFieldsValid(email, password)) return
+
+        toggleFields(false)
+        progressBar.visibility = android.view.View.VISIBLE
+
+        // Firebase Login with email and password
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                toggleFields(true)
+                progressBar.visibility = android.view.View.INVISIBLE
+                if (task.isSuccessful) {
+                    onSuccessfulLogin()
+                } else {
+                    onFailedLogin()
+                }
+            }
+    }
+
+    /**
      * Checks if the current user is logged in and has an verified email.
+     *
      * If not, it will show a dialog to the user to verify their email.
      * If the user has an verified email, it will go to the main activity.
      */
@@ -71,71 +100,85 @@ class LoginActivity : AppCompatActivity() {
     }
 
     /**
-     * Login Function
+     * Gets called when the login is successful.
+     *
+     * Shows a toast. It logs the login with [FirebaseAnalytics] and goes to the main activity.
+     */
+    private fun onSuccessfulLogin() {
+        Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+        analytics.logEvent(
+            FirebaseAnalytics.Event.LOGIN,
+            Bundle().apply {
+                putString(FirebaseAnalytics.Param.METHOD, "Email")
+                putString(FirebaseAnalytics.Param.SUCCESS, "true")
+            })
+        checkLoginStatus()
+        // TODO: update UI with the signed-in user's information
+    }
+
+    /**
+     * Gets called when the login is failed.
+     *
+     * It shows a toast with the error message and logs the login with [FirebaseAnalytics].
+     * It clears and changes the focus to the password field.
+     */
+    private fun onFailedLogin() {
+        // If sign in fails, display a message to the user.
+        emailField.error = "Email or Password is incorrect"
+        passwordField.text.clear()
+        passwordField.error = "Email or Password is incorrect"
+        passwordField.requestFocus()
+        Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
+        analytics.logEvent(
+            FirebaseAnalytics.Event.LOGIN,
+            Bundle().apply {
+                putString(FirebaseAnalytics.Param.METHOD, "Email")
+                putString(FirebaseAnalytics.Param.SUCCESS, "false")
+            })
+    }
+
+    /**
+     * Checks if the fields are valid.
+     *
+     * Gives feedback to the user if the fields are invalid.
+     * It also change the focus to the invalid field.
      * @param email Email
      * @param password Password (min 6 characters)
-     * @return void
+     * @return Boolean True if fields are valid, false if not
      */
-    fun login(email: String, password: String) {
+    private fun areFieldsValid(email: String, password: String): Boolean {
         if (email.isEmpty()) {
             emailField.requestFocus()
             emailField.error = "Email is required"
-            return
+            return false
         }
         // Check if email is valid
-        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
             emailField.requestFocus()
             emailField.error = "Email is invalid"
-            return
+            return false
         }
         if (password.isEmpty()) {
             passwordField.requestFocus()
             passwordField.error = "Password is required"
-            return
+            return false
         }
         if (password.length < MIN_PASSWORD_LENGTH) {
             passwordField.requestFocus()
             passwordField.error = "Password must be at least 6 characters"
-            return
+            return false
         }
+        return true
+    }
 
-        loginBtn.isEnabled = false
-        emailField.isEnabled = false
-        passwordField.isEnabled = false
-        progressBar.visibility = android.view.View.VISIBLE
-
-
-        // Firebase Login with email and password
-        auth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) { task ->
-                loginBtn.isEnabled = true
-                emailField.isEnabled = true
-                passwordField.isEnabled = true
-                progressBar.visibility = android.view.View.INVISIBLE
-                if (task.isSuccessful) {
-                    // Sign in success, TODO: update UI with the signed-in user's information
-                    Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
-                    analytics.logEvent(
-                        FirebaseAnalytics.Event.LOGIN,
-                        Bundle().apply { // TODO: Test this
-                            putString(FirebaseAnalytics.Param.METHOD, "Email")
-                            putString(FirebaseAnalytics.Param.SUCCESS, "true")
-                        })
-                    checkLoginStatus()
-                } else {
-                    // If sign in fails, display a message to the user.
-                    emailField.error = "Email or Password is incorrect"
-                    passwordField.text.clear()
-                    passwordField.error = "Email or Password is incorrect"
-                    passwordField.requestFocus()
-                    Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
-                    analytics.logEvent(
-                        FirebaseAnalytics.Event.LOGIN,
-                        Bundle().apply { // TODO: Test this
-                            putString(FirebaseAnalytics.Param.METHOD, "Email")
-                            putString(FirebaseAnalytics.Param.SUCCESS, "false")
-                        })
-                }
-            }
+    /**
+     * Toggles if the fields are enabled or disabled
+     * @param enabled Boolean to enable or disable the fields
+     * @return void
+     */
+    private fun toggleFields(enable: Boolean) {
+        loginBtn.isEnabled = enable
+        emailField.isEnabled = enable
+        passwordField.isEnabled = enable
     }
 }
