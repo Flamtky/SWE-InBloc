0c8803ac5b2b38aef30e3fa7891465016317d6ea
Flamtky <n.schumann@rz-online.de>
2022-03-01 02:15:40 +0100

    added check for already logged in

--- app/src/main/java/com/flamtkz_x/inbloc/LoginActivity.kt
+++ app/src/main/java/com/flamtkz_x/inbloc/LoginActivity.kt
@@ -19,6 +19,8 @@ class LoginActivity : AppCompatActivity() {
     private val emailField by lazy { findViewById<EditText>(R.id.emailMain) }
     private val passwordField by lazy { findViewById<EditText>(R.id.passwordMain) }
 
+    private val progressBar by lazy { findViewById<android.widget.ProgressBar>(R.id.progressBar1) }
+
     private lateinit var auth: FirebaseAuth
     private lateinit var analytics: FirebaseAnalytics
 
@@ -29,6 +31,16 @@ class LoginActivity : AppCompatActivity() {
         auth = FirebaseAuth.getInstance()
         analytics = FirebaseAnalytics.getInstance(this)
 
+        progressBar.visibility = android.view.View.INVISIBLE
+
+        // if already logged in, go to main activity
+        if (auth.currentUser != null) {
+            // TODO: go to main activity
+            val email = auth.currentUser!!.email
+            Toast.makeText(this, "Already logged in $email", Toast.LENGTH_SHORT).show()
+            auth.signOut()
+        }
+
         // Switch to Register Page
         switchToSignUp.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
@@ -37,18 +49,18 @@ class LoginActivity : AppCompatActivity() {
 
         // Login Button
         loginBtn.setOnClickListener {
-            login(emailField.text.toString(), passwordField.text.toString())
+            login(emailField.text.toString().trim(), passwordField.text.toString().trim())
         }
     }
 
     /**
      * Login Function
      * @param email Email
-     * @param password Password
+     * @param password Password (min 6 characters)
      * @return void
      */
     fun login(email: String, password: String) {
-        if (email.trim().isEmpty()) {
+        if (email.isEmpty()) {
             emailField.requestFocus()
             emailField.error = "Email is required"
             return
@@ -59,7 +71,7 @@ class LoginActivity : AppCompatActivity() {
             emailField.error = "Email is invalid"
             return
         }
-        if (password.trim().isEmpty()) {
+        if (password.isEmpty()) {
             passwordField.requestFocus()
             passwordField.error = "Password is required"
             return
@@ -69,22 +81,35 @@ class LoginActivity : AppCompatActivity() {
             passwordField.error = "Password must be at least 6 characters"
             return
         }
+
+        loginBtn.isEnabled = false
+        emailField.isEnabled = false
+        passwordField.isEnabled = false
+        progressBar.visibility = android.view.View.VISIBLE
+
+
         // Firebase Login with email and password
-        auth.signInWithEmailAndPassword(email.trim(), password.trim())
+        auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
+                loginBtn.isEnabled = true
+                emailField.isEnabled = true
+                passwordField.isEnabled = true
+                progressBar.visibility = android.view.View.INVISIBLE
                 if (task.isSuccessful) {
                     // Sign in success, TODO: update UI with the signed-in user's information
                     Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
-                    analytics.logEvent(FirebaseAnalytics.Event.LOGIN, Bundle().apply {
-                        putString(FirebaseAnalytics.Param.METHOD, "Email")
-                    })
+                    analytics.logEvent(
+                        FirebaseAnalytics.Event.LOGIN,
+                        Bundle().apply { // TODO: Test this
+                            putString(FirebaseAnalytics.Param.METHOD, "Email")
+                        })
                 } else {
                     // If sign in fails, display a message to the user.
                     emailField.error = "Email or Password is incorrect"
                     passwordField.error = "Email or Password is incorrect"
                     passwordField.text.clear()
                     Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
-                    analytics.logEvent("FAILED_LOGIN", Bundle().apply {
+                    analytics.logEvent("FAILED_LOGIN", Bundle().apply { // TODO: Test this
                         putString(FirebaseAnalytics.Param.METHOD, "Email")
                     })
                 }
