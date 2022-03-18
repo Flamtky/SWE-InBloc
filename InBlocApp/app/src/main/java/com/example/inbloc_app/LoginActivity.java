8ea055e3d2a2f6111e45a020ed1b876899b87585
Deedz <dennis.edler@fh-bielefeld.de>
2022-03-18 19:13:48 +0100

    get UserToken on Login

--- InBlocApp/app/src/main/java/com/example/inbloc_app/LoginActivity.java
+++ InBlocApp/app/src/main/java/com/example/inbloc_app/LoginActivity.java
@@ -11,17 +11,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.example.inbloc_app.data.Token;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GetTokenResult;
 
 public class LoginActivity extends AppCompatActivity {
 
     private static final String TAG = "";
     private FirebaseAuth mAuth;
+    static Token token;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,7 +73,17 @@ public class LoginActivity extends AppCompatActivity {
                             // Sign in success, update UI with the signed-in user's information
                             Log.i(TAG, "signInWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
-                            user.getIdToken(false);
+                            assert user != null;
+                            //TODO ProcessBar
+                            user.getIdToken(false).addOnSuccessListener(new OnSuccessListener<GetTokenResult>() {
+                                @Override
+                                public void onSuccess(GetTokenResult getTokenResult) {
+                                    String userID = getTokenResult.getToken();
+                                    Token.setToken(userID);
+                                    Log.i(TAG, "got User Token: " + userID);
+
+                                }
+                            });
 
                         } else {
                             // If sign in fails, display a message to the user.
