8d2c7b9e5b186cd83d3214e0ec81b12265c8f1cd
Deedz <dennis.edler@fh-bielefeld.de>
2022-03-18 16:37:00 +0100

    register and login in progress

--- InBlocApp/app/src/main/java/com/example/inbloc_app/LoginActivity.java
+++ InBlocApp/app/src/main/java/com/example/inbloc_app/LoginActivity.java
@@ -1,12 +1,18 @@
 package com.example.inbloc_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -48,6 +54,26 @@ public class LoginActivity extends AppCompatActivity {
         if(currentUser != null){
             currentUser.reload();
         }
+    }
+
+    public void signIn(String email, String password){
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                        }
+                    }
+                });
+
     }
 
     public void skipToHome(View view){
--- InBlocApp/app/src/main/java/com/example/inbloc_app/RegisterActivity.java
+++ InBlocApp/app/src/main/java/com/example/inbloc_app/RegisterActivity.java
@@ -2,25 +2,40 @@ package com.example.inbloc_app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private static final String TAG = "";
     private FirebaseAuth mAuth;
+    private EditText email;
+    private EditText password;
+    private EditText confirmPassword;
+    private boolean success = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         getSupportActionBar().hide();
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
         mAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.emailRegister);
+        password = findViewById(R.id.passwordRegister);
+        confirmPassword = findViewById(R.id.confirmPasswordRegister);
 
         Button returnToLoginBtn = (Button) findViewById(R.id.goToLogin_Register);
         returnToLoginBtn.setOnClickListener(new View.OnClickListener() {
@@ -35,17 +50,25 @@ public class RegisterActivity extends AppCompatActivity {
         registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                register(view);
+                String mail = email.getText().toString();
+                String pw = password.getText().toString();
+                String confPw = confirmPassword.getText().toString();
+
+                if (pw.equals(confPw)){
+                    createAccount(mail, pw);
+                    if (success == true){
+                        goToHome(view);
+                    } else {
+                        returnToLogin(view);
+                    }
+                }
             }
         });
 
 
     }
 
-    public void register(View view){
-
-
-
+    public void goToHome(View view){
         Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
     }
@@ -53,5 +76,28 @@ public class RegisterActivity extends AppCompatActivity {
     public void returnToLogin(View view){
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
+    }
+
+    public void createAccount(String email, String password){
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            success = true;
+//                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+//                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+//                                    Toast.LENGTH_SHORT).show();
+//                            updateUI(null);
+                        }
+                    }
+                });
     }
 }
