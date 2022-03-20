4d9bd48e69121d01c125476728e2d9774fc5602d
Dennis <dennis.edler@fh-bielefeld.de>
2022-03-20 19:58:37 +0100

    added Retrofit dependency, remote and interface

---
+++ InBlocApp/app/src/main/java/com/example/inbloc_app/retrofit/remote/ApiService.java
@@ -0,0 +1,12 @@
+package com.example.inbloc_app.retrofit.remote;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+import java.util.List;
+
+public interface ApiService {
+
+}
